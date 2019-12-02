9c8d93f2d9d996bbff23da0179046795019d9f96
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-12-02 09:46:48 -0500

    added the bamazon project

--- assets/javascript/projects.js
+++ assets/javascript/projects.js
@@ -1,36 +1,49 @@
 const projects = [{
-    title: "Daily Booze",
-    link: "https://dcornnell.github.io/WeatherDrink/",
-    github: "https://github.com/dcornnell/WeatherDrink",
-    description: "On visiting our page the user will be given a suggestion for a cocktail and a recipe. This recommendation will be based off the weather of the current users location. Looking at various weather statistics such as temperature, rain, snow or sunshine we will attempt to match a drink with ingredients that reflect those conditions.",
-    technologies: "A Group Project made with JavaScript, HTML, CSS. Hooks into the OpenWeatherMap and CocktailDB APIs. Geolocation services are done with the Google API.",
-    image: "/daily_booze.png"
-}, {
-    title: "Friend Finder!",
-    link: "https://murmuring-plains-79661.herokuapp.com/",
-    github: "https://github.com/dcornnell/FriendFinder",
-    description: "An app where the user answers a simple survey and is given a friend suggestion.",
-    technologies: "A Javascript, Express.js application, to practice servers and routing.",
-    image: "/friendfinder.png"
-}, {
-    title: "LIRI",
-    link: "https://drive.google.com/file/d/1PwOCO8v08U4ovMATU43leiNbt_g4mEtn/view",
-    github: "https://github.com/dcornnell/liri-node-app",
-    description: "A command line application that can search for movies, songs, and concert data.",
-    technologies: "A Javascript/Node application that uses multiple API and modules.",
-    image: "/liri.png"
-}, {
-    title: "Trivia Game",
-    link: "https://dcornnell.github.io/TriviaGame/",
-    github: "https://github.com/dcornnell/TriviaGame",
-    description: "A simple quiz game made to work with javascript timers, jquery click events and dom manipulation. The questions aren't mine, they are from the Earth Worm Jim video game.",
-    technologies: "Technologies used includes, Javascipt Jquery, CSS and Foundation.",
-    image: "/quiz_game.png"
-}, {
-    title: "Birds",
-    link: "http://cryptic-wildwood-1457.herokuapp.com/",
-    github: "https://github.com/dcornnell/birds",
-    description: "<i>Project from 2014</i> A one page site for creating animated birds. The birds move and respond to being clicked on. visitors to the site can create, update, and remove birds. Birds can have different sizes, colors and names.",
-    technologies: "Made with Ruby, Rails, Postgres, Haml, CSS, JavaScript, JSON",
-    image: "/bird.jpg"
-}, ]
+        title: "Daily Booze",
+        link: "https://dcornnell.github.io/WeatherDrink/",
+        github: "https://github.com/dcornnell/WeatherDrink",
+        description: "On visiting our page the user will be given a suggestion for a cocktail and a recipe. This recommendation will be based off the weather of the current users location. Looking at various weather statistics such as temperature, rain, snow or sunshine we will attempt to match a drink with ingredients that reflect those conditions.",
+        technologies: "A Group Project made with JavaScript, HTML, CSS. Hooks into the OpenWeatherMap and CocktailDB APIs. Geolocation services are done with the Google API.",
+        image: "/daily_booze.png"
+    },
+    {
+        title: "Bamazon",
+        link: "https://lit-oasis-31305.herokuapp.com/",
+        github: "https://github.com/dcornnell/bamazon",
+        description: `When the user vistis the site the can "purchase" products by added them to their cart. if they try to add more than are available or there input is invalid they will get an error once. once they are ready to check out the database is updated and the page is refreshed. On the manager side a user can add items, see a list of items that are low on inventory and add items to the inventory.`,
+        technologies: "Javascript, Express.js Sequelize.js, MySql deployed on Heroku",
+        image: "/bamazon.png"
+    },
+    {
+        title: "Friend Finder!",
+        link: "https://murmuring-plains-79661.herokuapp.com/",
+        github: "https://github.com/dcornnell/FriendFinder",
+        description: "An app where the user answers a simple survey and is given a friend suggestion.",
+        technologies: "A Javascript, Express.js application, to practice servers and routing.",
+        image: "/friendfinder.png"
+    },
+    {
+        title: "LIRI",
+        link: "https://drive.google.com/file/d/1PwOCO8v08U4ovMATU43leiNbt_g4mEtn/view",
+        github: "https://github.com/dcornnell/liri-node-app",
+        description: "A command line application that can search for movies, songs, and concert data.",
+        technologies: "A Javascript/Node application that uses multiple API and modules.",
+        image: "/liri.png"
+    },
+    {
+        title: "Trivia Game",
+        link: "https://dcornnell.github.io/TriviaGame/",
+        github: "https://github.com/dcornnell/TriviaGame",
+        description: "A simple quiz game made to work with javascript timers, jquery click events and dom manipulation. The questions aren't mine, they are from the Earth Worm Jim video game.",
+        technologies: "Technologies used includes, Javascipt Jquery, CSS and Foundation.",
+        image: "/quiz_game.png"
+    },
+    {
+        title: "Birds",
+        link: "http://cryptic-wildwood-1457.herokuapp.com/",
+        github: "https://github.com/dcornnell/birds",
+        description: "<i>Project from 2014</i> A one page site for creating animated birds. The birds move and respond to being clicked on. visitors to the site can create, update, and remove birds. Birds can have different sizes, colors and names.",
+        technologies: "Made with Ruby, Rails, Postgres, Haml, CSS, JavaScript, JSON",
+        image: "/bird.jpg"
+    }
+];
