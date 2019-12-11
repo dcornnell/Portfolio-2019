07267dec345e5bc66700cdd53c16e4058612f502
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-12-11 14:42:31 -0500

    fixed some spelling errors

--- assets/javascript/projects.js
+++ assets/javascript/projects.js
@@ -2,24 +2,24 @@ const projects = [{
         title: "Daily Booze",
         link: "https://dcornnell.github.io/WeatherDrink/",
         github: "https://github.com/dcornnell/WeatherDrink",
-        description: "On visiting our page the user will be given a suggestion for a cocktail and a recipe. This recommendation will be based off the weather of the current users location. Looking at various weather statistics such as temperature, rain, snow or sunshine we will attempt to match a drink with ingredients that reflect those conditions.",
-        technologies: "A Group Project made with JavaScript, HTML, CSS. Hooks into the OpenWeatherMap and CocktailDB APIs. Geolocation services are done with the Google API.",
+        description: "An application that will give a suggestion for a cocktail and a recipe. This recommendation is based off the weather of the current users location. Looking at various weather statistics such as temperature, rain, snow or sunshine it will attempt to match a drink with ingredients that reflect those conditions.",
+        technologies: "A Group Project made with JavaScript, HTML, CSS. It connects to the OpenWeatherMap and CocktailDB APIs. Geolocation services are done with the Google API.",
         image: "/daily_booze.png"
     },
     {
         title: "Job Locator",
         link: "https://sheltered-peak-49763.herokuapp.com/",
         github: "https://github.com/dcornnell/Job-Locator",
-        description: "A Web site for job searchers that displays a map of a chosen area. The user will be able to select search parameters distance, job position, location, etc. The map will show the locations of job postings in the specified area. If users log in their previous searches will be saved.",
-        technologies: "A Group Project made with JavaScript, Node.js Express.js HTML, CSS. Modules we used include: bcrypt passport, and express sessions. API used include: Jooble API, Google Maps and Places",
+        description: "A Web site for job searchers that displays a map of a chosen area. The user will be able to select search parameters distance, job position, location, etc. The map will show the locations of job postings in the specified area.",
+        technologies: "A Group Project made with JavaScript, Node.js Express.js HTML, CSS. Modules we used include: bcrypt, passport, and express sessions. API used include: Jooble API, Google Maps and Places.",
         image: "/job.png"
     },
     {
         title: "Bamazon",
         link: "https://lit-oasis-31305.herokuapp.com/",
         github: "https://github.com/dcornnell/bamazon",
-        description: `When the user vistis the site the can "purchase" products by added them to their cart. if they try to add more than are available or there input is invalid they will get an error once. once they are ready to check out the database is updated and the page is refreshed. On the manager side a user can add items, see a list of items that are low on inventory and add items to the inventory.`,
-        technologies: "Javascript, Express.js Sequelize.js, MySql deployed on Heroku",
+        description: `When the user vistis the site they can "purchase" products by added them to their cart. If they try to add more than are available or there input is invalid they will get an error once. Once they are ready to check out the database is updated and the page is refreshed. On the manager side a user can add items, see a list of items that are low on inventory and add items to the inventory.`,
+        technologies: "HTMl, CSS, JavaScript, Express.js Sequelize.js and MySql are used. deployed on Heroku with JAWSDB.",
         image: "/bamazon.png"
     },
     {
@@ -27,7 +27,7 @@ const projects = [{
         link: "https://murmuring-plains-79661.herokuapp.com/",
         github: "https://github.com/dcornnell/FriendFinder",
         description: "An app where the user answers a simple survey and is given a friend suggestion.",
-        technologies: "A Javascript, Express.js application, to practice servers and routing.",
+        technologies: "A JavaScript, Express.js application, to practice servers and routing.",
         image: "/friendfinder.png"
     },
     {
@@ -35,15 +35,15 @@ const projects = [{
         link: "https://drive.google.com/file/d/1PwOCO8v08U4ovMATU43leiNbt_g4mEtn/view",
         github: "https://github.com/dcornnell/liri-node-app",
         description: "A command line application that can search for movies, songs, and concert data.",
-        technologies: "A Javascript/Node application that uses multiple API and modules.",
+        technologies: "A JavaScript/Node application that uses multiple API and modules.",
         image: "/liri.png"
     },
     {
         title: "Trivia Game",
         link: "https://dcornnell.github.io/TriviaGame/",
         github: "https://github.com/dcornnell/TriviaGame",
-        description: "A simple quiz game made to work with javascript timers, jquery click events and dom manipulation. The questions aren't mine, they are from the Earth Worm Jim video game.",
-        technologies: "Technologies used includes, Javascipt Jquery, CSS and Foundation.",
+        description: "A simple quiz game made to work with JavaScript timers, JQuery click events and dom manipulation. The questions aren't mine, they are from the Earth Worm Jim video game.",
+        technologies: "Technologies used includes, Javascipt JQuery, CSS and Foundation.",
         image: "/quiz_game.png"
     },
     {
