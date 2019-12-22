eaa71f4beaf18abe3d3bc3cdad7a9c04a7b01679
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-12-22 15:50:16 -0500

    added the clicky game homework

--- assets/javascript/projects.js
+++ assets/javascript/projects.js
@@ -1,65 +1,93 @@
-const projects = [{
-        title: "Daily Booze",
-        link: "https://dcornnell.github.io/WeatherDrink/",
-        github: "https://github.com/dcornnell/WeatherDrink",
-        description: "An application that will give a suggestion for a cocktail and a recipe. This recommendation is based off the weather of the current users location. Looking at various weather statistics such as temperature, rain, snow or sunshine it will attempt to match a drink with ingredients that reflect those conditions.",
-        technologies: "A Group Project made with JavaScript, HTML, CSS. It connects to the OpenWeatherMap and CocktailDB APIs. Geolocation services are done with the Google API.",
-        image: "/daily_booze.png"
-    },
-    {
-        title: "Job Locator",
-        link: "https://sheltered-peak-49763.herokuapp.com/",
-        github: "https://github.com/dcornnell/Job-Locator",
-        description: "A Web site for job searchers that displays a map of a chosen area. The user will be able to select search parameters distance, job position, location, etc. The map will show the locations of job postings in the specified area.",
-        technologies: "A Group Project made with JavaScript, Node.js Express.js HTML, CSS. Modules we used include: bcrypt, passport, and express sessions. API used include: Jooble API, Google Maps and Places.",
-        image: "/job.png"
-    },
-    {
-        title: "Weekly World Scrape",
-        link: "https://weeklyworldscraper.herokuapp.com/",
-        github: "https://github.com/dcornnell/NewsScraper",
-        description: "A Web site that scrapes WeeklyWorldNews.com using Cheerio.js for news stories.  Users can view stories and leave comment on them.  ",
-        technologies: "HTML, CSS, JavaScript, Node, MongoDB and Mongoose Cheerio. Deployed on heroku",
-        image: "/wwscrape.png"
-    },
-    {
-        title: "Bamazon",
-        link: "https://lit-oasis-31305.herokuapp.com/",
-        github: "https://github.com/dcornnell/bamazon",
-        description: `When the user vistis the site they can "purchase" products by added them to their cart. If they try to add more than are available or there input is invalid they will get an error once. Once they are ready to check out the database is updated and the page is refreshed. On the manager side a user can add items, see a list of items that are low on inventory and add items to the inventory.`,
-        technologies: "HTMl, CSS, JavaScript, Express.js Sequelize.js and MySql are used. deployed on Heroku with JAWSDB.",
-        image: "/bamazon.png"
-    },
-    {
-        title: "Friend Finder!",
-        link: "https://murmuring-plains-79661.herokuapp.com/",
-        github: "https://github.com/dcornnell/FriendFinder",
-        description: "An app where the user answers a simple survey and is given a friend suggestion.",
-        technologies: "A JavaScript, Express.js application, to practice servers and routing.",
-        image: "/friendfinder.png"
-    },
-    {
-        title: "LIRI",
-        link: "https://drive.google.com/file/d/1PwOCO8v08U4ovMATU43leiNbt_g4mEtn/view",
-        github: "https://github.com/dcornnell/liri-node-app",
-        description: "A command line application that can search for movies, songs, and concert data.",
-        technologies: "A JavaScript/Node application that uses multiple API and modules.",
-        image: "/liri.png"
-    },
-    {
-        title: "Trivia Game",
-        link: "https://dcornnell.github.io/TriviaGame/",
-        github: "https://github.com/dcornnell/TriviaGame",
-        description: "A simple quiz game made to work with JavaScript timers, JQuery click events and dom manipulation. The questions aren't mine, they are from the Earth Worm Jim video game.",
-        technologies: "Technologies used includes, Javascipt JQuery, CSS and Foundation.",
-        image: "/quiz_game.png"
-    },
-    {
-        title: "Birds",
-        link: "http://cryptic-wildwood-1457.herokuapp.com/",
-        github: "https://github.com/dcornnell/birds",
-        description: "<i>Project from 2014</i> A one page site for creating animated birds. The birds move and respond to being clicked on. visitors to the site can create, update, and remove birds. Birds can have different sizes, colors and names.",
-        technologies: "Made with Ruby, Rails, Postgres, Haml, CSS, JavaScript, JSON",
-        image: "/bird.jpg"
-    }
+const projects = [
+  {
+    title: "Daily Booze",
+    link: "https://dcornnell.github.io/WeatherDrink/",
+    github: "https://github.com/dcornnell/WeatherDrink",
+    description:
+      "An application that will give a suggestion for a cocktail and a recipe. This recommendation is based off the weather of the current users location. Looking at various weather statistics such as temperature, rain, snow or sunshine it will attempt to match a drink with ingredients that reflect those conditions.",
+    technologies:
+      "A Group Project made with JavaScript, HTML, CSS. It connects to the OpenWeatherMap and CocktailDB APIs. Geolocation services are done with the Google API.",
+    image: "/daily_booze.png"
+  },
+  {
+    title: "Job Locator",
+    link: "https://sheltered-peak-49763.herokuapp.com/",
+    github: "https://github.com/dcornnell/Job-Locator",
+    description:
+      "A Web site for job searchers that displays a map of a chosen area. The user will be able to select search parameters distance, job position, location, etc. The map will show the locations of job postings in the specified area.",
+    technologies:
+      "A Group Project made with JavaScript, Node.js Express.js HTML, CSS. Modules we used include: bcrypt, passport, and express sessions. API used include: Jooble API, Google Maps and Places.",
+    image: "/job.png"
+  },
+  {
+    title: "Clicky Game",
+    link: "https://dcornnell.github.io/clicky-game/",
+    github: "https://github.com/dcornnell/clicky-game",
+    description:
+      "A Memory Game. The user clicks on a previously unclicked image. if a user clicks on the same image twice the game is over. the images move after every click!",
+    technologies:
+      "an App created with React.js To learn the basics of props and components. As well as the state object. Technologies used include, Javascript, Node, React.js, HTML and CSS",
+    image: "/clicky.png"
+  },
+
+  {
+    title: "Weekly World Scrape",
+    link: "https://weeklyworldscraper.herokuapp.com/",
+    github: "https://github.com/dcornnell/NewsScraper",
+    description:
+      "A Web site that scrapes WeeklyWorldNews.com using Cheerio.js for news stories.  Users can view stories and leave comment on them.  ",
+    technologies:
+      "HTML, CSS, JavaScript, Node, MongoDB and Mongoose Cheerio. Deployed on heroku",
+    image: "/wwscrape.png"
+  },
+  {
+    title: "Bamazon",
+    link: "https://lit-oasis-31305.herokuapp.com/",
+    github: "https://github.com/dcornnell/bamazon",
+    description: `When the user vistis the site they can "purchase" products by added them to their cart. If they try to add more than are available or there input is invalid they will get an error once. Once they are ready to check out the database is updated and the page is refreshed. On the manager side a user can add items, see a list of items that are low on inventory and add items to the inventory.`,
+    technologies:
+      "HTMl, CSS, JavaScript, Express.js Sequelize.js and MySql are used. deployed on Heroku with JAWSDB.",
+    image: "/bamazon.png"
+  },
+  {
+    title: "Friend Finder!",
+    link: "https://murmuring-plains-79661.herokuapp.com/",
+    github: "https://github.com/dcornnell/FriendFinder",
+    description:
+      "An app where the user answers a simple survey and is given a friend suggestion.",
+    technologies:
+      "A JavaScript, Express.js application, to practice servers and routing.",
+    image: "/friendfinder.png"
+  },
+  {
+    title: "LIRI",
+    link:
+      "https://drive.google.com/file/d/1PwOCO8v08U4ovMATU43leiNbt_g4mEtn/view",
+    github: "https://github.com/dcornnell/liri-node-app",
+    description:
+      "A command line application that can search for movies, songs, and concert data.",
+    technologies:
+      "A JavaScript/Node application that uses multiple API and modules.",
+    image: "/liri.png"
+  },
+  {
+    title: "Trivia Game",
+    link: "https://dcornnell.github.io/TriviaGame/",
+    github: "https://github.com/dcornnell/TriviaGame",
+    description:
+      "A simple quiz game made to work with JavaScript timers, JQuery click events and dom manipulation. The questions aren't mine, they are from the Earth Worm Jim video game.",
+    technologies:
+      "Technologies used includes, Javascipt JQuery, CSS and Foundation.",
+    image: "/quiz_game.png"
+  },
+  {
+    title: "Birds",
+    link: "http://cryptic-wildwood-1457.herokuapp.com/",
+    github: "https://github.com/dcornnell/birds",
+    description:
+      "<i>Project from 2014</i> A one page site for creating animated birds. The birds move and respond to being clicked on. visitors to the site can create, update, and remove birds. Birds can have different sizes, colors and names.",
+    technologies:
+      "Made with Ruby, Rails, Postgres, Haml, CSS, JavaScript, JSON",
+    image: "/bird.jpg"
+  }
 ];
